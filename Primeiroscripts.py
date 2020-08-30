c1deb5f7daf27574cd2dcda923be870ebc31c4c9
JoadsonRocha <joadsonrocharr@gmail.com>
2020-08-30 15:19:29 -0400

    primeirosscripts

---
+++ Primeiroscripts.py
@@ -0,0 +1,11 @@
+print('ola, mundo')
+print(7+4)
+print('7'+'4') 
+print('Olá',5)
+
+nome= input('Qual seu nome? ')
+idade= input('Qual sua Idade? ')
+peso = input('Qual seu peso? ')
+print('Seu nome é',nome, 'Sua idade é', idade,'e seu peso é', peso) 
+
+ 
