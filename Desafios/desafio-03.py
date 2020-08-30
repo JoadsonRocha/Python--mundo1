6b5a8d8ed4b701724f472f3fc6c2dd16140f7ccc
JoadsonRocha <joadsonrocharr@gmail.com>
2020-08-30 16:21:32 -0400

    tipos primitivos

--- Desafios/desafio-03.py
+++ Desafios/desafio-03.py
@@ -1,3 +1,5 @@
-n1= int(input('Digite um número: '))
-n2= int(input('Digite outro número: '))
-print('A soma dos Números é:', n1+n2)
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1+n2
+#print('A soma entre:',n1,'e',n2,'Vale:', s)
+print('A soma entre {} e {} Vale: {}'.format(n1 , n2, s))  
